ff7ea1f44e6fea8c7e716be6e8383c6f6a971157
jsm1129 <jamja0513@sookmyung.ac.kr>
2025-12-23 20:16:59 +0900

    [feature] boddy challenge 상단 UI 구현 #6

--- lib/features/challenge/pages/challenge_page.dart
+++ lib/features/challenge/pages/challenge_page.dart
@@ -1,14 +1,49 @@
-//예시 페이지 구조
-
-// features/home/pages/home_page.dart
-
 import 'package:flutter/material.dart';
+import '../widgets/challenge_scope_toggle.dart';
 
 class ChallengePage extends StatelessWidget {
   const ChallengePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Challenge Page'));
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        titleSpacing: 16,
+        centerTitle: false,
+        title: const Text(
+          '바디 챌린지',
+          style: TextStyle(
+            fontFamily: 'Pretendard',
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            height: 1.0, // line-height 100%
+            letterSpacing: 0,
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: Image.asset(
+              'asset/challenge/shop.png',
+              width: 24,
+              height: 24,
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            ChallengeScopeToggle(),
+            SizedBox(height: 24),
+          ],
+        ),
+      ),
+    );
   }
 }
