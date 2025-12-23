acfa52bea4beb2d007473209f4a7611d631d4c7a
jsm1129 <jamja0513@sookmyung.ac.kr>
2025-12-23 22:11:44 +0900

    [fix] UI 가려져서 안보임 해결 #6

--- lib/features/challenge/widgets/challenge_scope_toggle.dart
+++ lib/features/challenge/widgets/challenge_scope_toggle.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ChallengeScopeToggle extends StatelessWidget {
+  /// true = 개인, false = 그룹
   final bool isPersonalSelected;
   final ValueChanged<bool> onChanged;
 
@@ -14,7 +15,7 @@ class ChallengeScopeToggle extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 49,
-      padding: const EdgeInsets.symmetric(horizontal: 17),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       decoration: const BoxDecoration(
         color: Color(0xFFF8F8F8),
       ),
@@ -27,6 +28,7 @@ class ChallengeScopeToggle extends StatelessWidget {
               fontSize: 12,
               fontWeight: FontWeight.w600,
               height: 1.0,
+              letterSpacing: 0,
               color: Colors.black,
             ),
           ),
@@ -35,26 +37,16 @@ class ChallengeScopeToggle extends StatelessWidget {
 
           Row(
             children: [
-              MouseRegion(
-                cursor: SystemMouseCursors.click,
-                child: GestureDetector(
-                  onTap: () => onChanged(true),
-                  child: _ScopeButton(
-                    label: '개인',
-                    isSelected: isPersonalSelected,
-                  ),
-                ),
+              _ScopeButton(
+                label: '개인',
+                isSelected: isPersonalSelected,
+                onTap: () => onChanged(true),
               ),
               const SizedBox(width: 4),
-              MouseRegion(
-                cursor: SystemMouseCursors.click,
-                child: GestureDetector(
-                  onTap: () => onChanged(false),
-                  child: _ScopeButton(
-                    label: '그룹',
-                    isSelected: !isPersonalSelected,
-                  ),
-                ),
+              _ScopeButton(
+                label: '그룹',
+                isSelected: !isPersonalSelected,
+                onTap: () => onChanged(false),
               ),
             ],
           ),
@@ -67,36 +59,45 @@ class ChallengeScopeToggle extends StatelessWidget {
 class _ScopeButton extends StatelessWidget {
   final String label;
   final bool isSelected;
+  final VoidCallback onTap;
 
   const _ScopeButton({
     required this.label,
     required this.isSelected,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 64,
-      height: 25,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        color: isSelected ? Colors.black : Colors.white,
-        borderRadius: BorderRadius.circular(20),
-        border: isSelected
-            ? null
-            : Border.all(
-                width: 0.7,
-                color: const Color(0xFFDBDBDB),
-              ),
-      ),
-      child: Text(
-        label,
-        style: TextStyle(
-          fontFamily: 'Pretendard',
-          fontSize: 12,
-          fontWeight: FontWeight.w500,
-          height: 1.0,
-          color: isSelected ? Colors.white : const Color(0xFFDBDBDB),
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          width: 64,
+          height: 25,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: isSelected ? Colors.black : Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            border: isSelected
+                ? null
+                : Border.all(
+                    width: 0.7,
+                    color: const Color(0xFFDBDBDB),
+                  ),
+          ),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontFamily: 'Pretendard',
+              fontSize: 12,
+              fontWeight: FontWeight.w500,
+              height: 1.0,
+              color:
+                  isSelected ? Colors.white : const Color(0xFFDBDBDB),
+            ),
+          ),
         ),
       ),
     );
