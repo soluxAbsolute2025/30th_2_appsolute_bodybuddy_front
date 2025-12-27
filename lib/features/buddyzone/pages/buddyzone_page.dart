6039a7cdfec833185eaf0fab97b22477920cc706
EunHaSong <shaeun03290@sookmyung.ac.kr>
2025-12-27 16:00:13 +0900

    [Feat] BuddyZone 기본 구조 제작 및 탭 구조 설정

--- lib/features/buddyzone/pages/buddyzone_page.dart
+++ lib/features/buddyzone/pages/buddyzone_page.dart
@@ -1,14 +1,119 @@
-//예시 페이지 구조
-
 // features/home/pages/home_page.dart
 
 import 'package:flutter/material.dart';
 
-class BuddyZonePage extends StatelessWidget {
+class BuddyZonePage extends StatefulWidget {
   const BuddyZonePage({super.key});
 
   @override
+  State<BuddyZonePage> createState() => _BuddyZoneState();
+}
+
+class _BuddyZoneState extends State<BuddyZonePage> {
+  int _isBuddySelectIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('BuddyZone Page'));
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        titleSpacing: 0,
+        title: SizedBox(
+          height: 60.0,
+          child: const Padding(
+            padding: EdgeInsets.fromLTRB(16.0, 19.0, 16.0, 17.0),
+            child: Text(
+              '버디존',
+              style: TextStyle(
+                fontFamily: 'Pretendard',
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                height: 1.0,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 49.0,
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _isBuddySelectIndex = 0;
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border(
+                          bottom: BorderSide(
+                            width: 2.0,
+                            color: _isBuddySelectIndex == 0
+                                ? Color(0xFF1AEDB1)
+                                : Colors.transparent,
+                          ),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          '피드',
+                          style: TextStyle(
+                            fontFamily: 'Pretendard',
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _isBuddySelectIndex = 1;
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border(
+                          bottom: BorderSide(
+                            width: 2.0,
+                            color: _isBuddySelectIndex == 1
+                                ? Color(0xFF1AEDB1)
+                                : Colors.transparent,
+                          ),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          '친구',
+                          style: TextStyle(
+                            fontFamily: 'Pretendard',
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
