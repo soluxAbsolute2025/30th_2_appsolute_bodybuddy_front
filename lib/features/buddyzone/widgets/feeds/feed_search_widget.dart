e6166a806406e0a7a2a2d0278f40338ec9dbf117
EunHaSong <shaeun03290@sookmyung.ac.kr>
2025-12-29 03:33:48 +0900

    [Feat] 피드 탭 기본 피드 형태 제작 완료

---
+++ lib/features/buddyzone/widgets/feeds/feed_search_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FeedSearchWidget extends StatelessWidget {
+  const FeedSearchWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[SizedBox(height: 10.0)]);
+  }
+}
