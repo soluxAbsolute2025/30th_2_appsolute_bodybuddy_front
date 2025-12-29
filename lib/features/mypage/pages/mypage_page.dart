f5a010843cb5f31abd4012240fe403d3aaa741ca
EunHaSong <shaeun03290@sookmyung.ac.kr>
2025-12-30 06:29:21 +0900

    [Feat] 마이페이지 구조 구현 완료

--- lib/features/mypage/pages/mypage_page.dart
+++ lib/features/mypage/pages/mypage_page.dart
@@ -3,12 +3,40 @@
 // features/home/pages/home_page.dart
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../common/widgets/main_appbar.dart';
+import '../widgets/mypage_level_widget.dart';
+import '../widgets/mypage_moem_widget.dart';
+import '../widgets/mypage_profile_widget.dart';
 
 class MypagePage extends StatelessWidget {
   const MypagePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Mypage Page'));
+    return Scaffold(
+      backgroundColor: Color(0xFFF8F8F8),
+      appBar: MainAppbar(
+        navIndex: 4,
+        titleText: '마이페이지',
+        imageUrl: 'assets/images/common/my.svg',
+        buttonText: '친구 추가',
+      ),
+      body: Container(
+        color: Colors.white,
+        margin: EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            MypageProfileWidget(),
+            SizedBox(height: 5.0),
+            MypageLevelWidget(),
+            SizedBox(height: 30.0),
+            MypageMoemWidget(),
+            SizedBox(height: 16.0),
+          ],
+        ),
+      ),
+    );
   }
 }
