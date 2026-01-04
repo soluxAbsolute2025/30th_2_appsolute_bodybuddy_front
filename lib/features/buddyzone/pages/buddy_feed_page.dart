3302aaa6166e300d8d12266e6531e2331d99cd0c
EunHaSong <shaeun03290@sookmyung.ac.kr>
2026-01-04 17:34:25 +0900

    Feat. 피드 댓글 상세 페이지 제작 완료

--- lib/features/buddyzone/pages/buddy_feed_page.dart
+++ lib/features/buddyzone/pages/buddy_feed_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import '../widgets/feeds/feed_only_widget.dart';
 import '../widgets/feeds/feed_search_widget.dart';
 import '../widgets/feeds/feed_hottag_widget.dart';
+import '../widgets/feeds/feed_frame_widget.dart';
+import './subPages/sub_feed_pages.dart';
 
 class BuddyFeedPage extends StatelessWidget {
   const BuddyFeedPage({super.key});
@@ -17,9 +18,9 @@ class BuddyFeedPage extends StatelessWidget {
           SizedBox(height: 16.0),
           FeedHottagWidget(),
           SizedBox(height: 24.0),
-          FeedOnlyWidget(),
+          FeedFrameWidget(),
           SizedBox(height: 16.0),
-          FeedOnlyWidget(),
+          FeedFrameWidget(),
           SizedBox(height: 16.0),
         ],
       ),
