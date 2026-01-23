659575f3209ee960a8cafa940b28892f3e9f9a91
jsm1129 <jamja0513@sookmyung.ac.kr>
2026-01-23 10:58:58 +0900

    [feat] 그룹 챌린지 전체 보기 모달창 구현

--- lib/features/challenge/data/dummy_group_challenges.dart
+++ lib/features/challenge/data/dummy_group_challenges.dart
@@ -37,4 +37,109 @@ const List<GroupChallenge> dummyGroupChallenges = [
       ),
     ],
   ),
+  GroupChallenge(
+    title: '친구들과 다이어트',
+    imageUrl: 'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
+    rank: 2,
+    remainDays: 7,
+    isRecruiting: false,
+    members: [
+      GroupMember(
+        id: 'me',
+        profileImageUrl:
+            'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
+        isMe: true,
+      ),
+      GroupMember(
+        id: 'u1',
+        profileImageUrl:
+            'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
+      ),
+      GroupMember(
+        id: 'u2',
+        profileImageUrl:
+            'null',
+      ),
+      GroupMember(
+        id: 'u3',
+        profileImageUrl:
+            'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
+      ),
+      GroupMember(
+        id: 'u4',
+        profileImageUrl:
+            'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
+      ),
+    ],
+  ),
+  GroupChallenge(
+    title: '친구들과 다이어트',
+    imageUrl: 'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
+    rank: 2,
+    remainDays: 7,
+    isRecruiting: false,
+    members: [
+      GroupMember(
+        id: 'me',
+        profileImageUrl:
+            'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
+        isMe: true,
+      ),
+      GroupMember(
+        id: 'u1',
+        profileImageUrl:
+            'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
+      ),
+      GroupMember(
+        id: 'u2',
+        profileImageUrl:
+            'null',
+      ),
+      GroupMember(
+        id: 'u3',
+        profileImageUrl:
+            'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
+      ),
+      GroupMember(
+        id: 'u4',
+        profileImageUrl:
+            'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
+      ),
+    ],
+  ),
+  GroupChallenge(
+    title: '친구들과 다이어트',
+    imageUrl: 'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
+    rank: 2,
+    remainDays: 7,
+    isRecruiting: false,
+    members: [
+      GroupMember(
+        id: 'me',
+        profileImageUrl:
+            'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
+        isMe: true,
+      ),
+      GroupMember(
+        id: 'u1',
+        profileImageUrl:
+            'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
+      ),
+      GroupMember(
+        id: 'u2',
+        profileImageUrl:
+            'null',
+      ),
+      GroupMember(
+        id: 'u3',
+        profileImageUrl:
+            'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
+      ),
+      GroupMember(
+        id: 'u4',
+        profileImageUrl:
+            'https://cdn-icons-png.flaticon.com/512/2965/2965567.png',
+      ),
+    ],
+  ),
 ];
