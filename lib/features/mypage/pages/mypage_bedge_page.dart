2166578789fd0ea029f306d7ca45b33e43755946
EunHaSong <shaeun03290@sookmyung.ac.kr>
2026-01-25 05:13:31 +0900

    Update mypage_bedge_page.dart

--- lib/features/mypage/pages/mypage_bedge_page.dart
+++ lib/features/mypage/pages/mypage_bedge_page.dart
@@ -1,20 +1,16 @@
 import 'package:bodybuddy_frontend/common/widgets/sub_appbar.dart';
 import 'package:flutter/material.dart';
 
-
 class MypageBedgePage extends StatefulWidget {
   const MypageBedgePage({super.key});
 
   @override
   State<MypageBedgePage> createState() => _MypageBedgePageState();
-
 }
 
 class _MypageBedgePageState extends State<MypageBedgePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: SubAppbar(titleText: '뱃지 컬렉션',),
-
-    );
+    return Scaffold(appBar: SubAppbar(titleText: '뱃지 컬렉션'));
+  }
 }
